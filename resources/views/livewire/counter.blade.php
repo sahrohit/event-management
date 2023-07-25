cc8ac03a0287d7049d86febdfedcbdff59b6d846
Rohit Sah <sahrohit9586@gmail.com>
2023-07-26 01:01:31 +0545

    init

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-row gap-4">
+    <button class="p-4" wire:click="decrement">-</button>
+    <h1>{{ $count }}</h1>
+    <button class="p-4" wire:click="increment">+</button>
+</div>
